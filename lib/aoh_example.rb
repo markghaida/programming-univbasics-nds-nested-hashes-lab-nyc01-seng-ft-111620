bea660398d1a5456206fb77ec419d09b9c0dc01d
Mark G <markghaida@gmail.com>
2020-10-29 00:12:01 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,6 +22,7 @@ def literal_aoh
   people = [
     { name:  "Don Gately", occupation: "Live-in Staff" }, 
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
     
     ]
   
