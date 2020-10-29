500e1cc86c8cf0720e9fb4111cedb9afba475faf
Mark G <markghaida@gmail.com>
2020-10-29 00:10:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,8 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
   
   people = [
-    DON_G, JOELLE_VD, PAT_M , KATE_G, ]
+    DON_G, JOELLE_VD, PAT_M , KATE_G, BRUCE_G
+    ]
 end
 
 def literal_aoh
